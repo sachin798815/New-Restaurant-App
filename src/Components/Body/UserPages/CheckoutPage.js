3052858b3ab88c9a95b706a521dfc3727be0fa3a
sachin798815 <sachin798815@gmail.com>
2025-01-18 16:21:38 +0530

    user checkout

---
+++ src/Components/Body/UserPages/CheckoutPage.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+const CheckoutPage = () => {
+  const [orderList, setOrderList] = useState([]);
+  const userName = localStorage.getItem("name");
+  const [totalAmount, setTotalAmount] = useState(0);
+  const history=useHistory();
+
+  useEffect(() => {
+    fetch(
+      `https://restaurant-delivery-app-5c344-default-rtdb.firebaseio.com/orders/${userName}.json`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          const itemList = Object.keys(data).map((key) => ({
+            ...data[key],
+            id: key,
+          }));
+          setOrderList(itemList);
+          setTotalAmount(
+            itemList.reduce((acc, item) => Number(acc) + Number(item.price), 0)
+          );
+        }
+      })
+      .catch((error) => console.error(error));
+  }, [userName]);
+
+  const placeOrderHandler=(e)=>{
+    e.preventDefault();
+    fetch(
+        `https://restaurant-delivery-app-5c344-default-rtdb.firebaseio.com/accepted/${userName}.json`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userName: userName,
+            orderList: orderList,
+            totalAmount:totalAmount,
+            orderStatus:"pending"
+          }),
+        }
+      );
+      fetch(
+        `https://restaurant-delivery-app-5c344-default-rtdb.firebaseio.com/orders/${userName}.json`,
+        {
+          method: "DELETE"
+        }
+      )
+      history.push("/orderstatus")
+  }
+  return (
+    <Container className="border rounded text-center container-sm p-2 mt-3 shadow p-3 mb-5 bg-white rounded">
+        <h1 className="text-center my-4">Checkout</h1>
+      <Form>
+        {orderList.map((order) => (
+          <li key={order.id} className="m-3">
+            <span>{order.recipe} </span>
+            <span>Rs {order.price}</span>
+          </li>
+        ))}
+        <div className="fs-5 mb-3">Total amount : {totalAmount} Rs</div>
+        <Form.Label>Address</Form.Label>
+        <Form.Control type="text" placeholder="Enter your address" />
+        <Button type="submit" className="m-3" onClick={placeOrderHandler}>Place Order</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default CheckoutPage;
